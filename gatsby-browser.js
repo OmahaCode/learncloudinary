b43700441adc272d20c553e9b459991423057d17
Neil Kristian <nhumphrey@unomaha.edu>
2019-10-08 14:49:18 -0500

    update browser smooth scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// Make scroll behavior of internal links smooth
+exports.onClientEntry = () => {
+  const SmoothScroll = require('smooth-scroll');
+  new SmoothScroll('a[href*="#"]', {
+    offset() {
+      return document.querySelector('#header').offsetHeight;
+    },
+  });
+};
+
  // You can delete this file if you're not using it
