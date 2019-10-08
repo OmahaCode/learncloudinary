940f7d61c209674c1a15aae4e5ed6ac63bf054a1
Neil <neil@wickfire.com>
2019-10-08 00:31:59 -0500

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'cloudinaryapi',
+        short_name: 'cloudinaryapi',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
